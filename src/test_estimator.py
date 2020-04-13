0be0cf3611815edf24e6fc56f74376792a6c9371
Oswago Hesborn <oswago76@gmail.com>
2020-04-13 16:24:03 +0300

    added input prompt check

--- src/test_estimator.py
+++ src/test_estimator.py
@@ -5,7 +5,21 @@ import math
 
 class TestPower(unittest.TestCase):
     def test_divide(self):             
-        self.assertEqual(math.pow(2, 3), 8)        
+        self.assertEqual(math.pow(2, 3), 8)
+    
+    def test_add(self):             
+        self.assertEqual(math.add(10, 5), 15)
+    
+    def test_multiply(self):             
+        self.assertEqual(math.multiply(10, 5), 50)
+        
+    def test_divide(self):             
+        self.assertEqual(math.divide(10, 5), 2)
+    
+    def test_prompt(self, capsys, monkeypatch):
+        monkeypatch.setattr('path.to.yourmodule.input', lambda: 'no')
+        val = input(bear=..., printer=...)
+        assert not val        
         
        
 if __name__ == '__main__':
