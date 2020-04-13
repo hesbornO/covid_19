c75899ffed228fe163d0c3a8e793228668dccf1c
Oswago Hesborn <oswago76@gmail.com>
2020-04-13 22:38:32 +0300

    fdfs

--- src/test_estimator.py
+++ src/test_estimator.py
@@ -7,26 +7,9 @@ from unittest import TestCase
 
 
 class EstimatorTest(unittest.TestCase):
-    # def test_divide(self):             
-    #     self.assertEqual(calc.pow(2, 3), 8)
-    
-    # def test_add(self):             
-    #     self.assertEqual(calc.add(10, 5), 15)
-    
-    # def test_multiply(self):             
-    #     self.assertEqual(calc.multiply(10, 5), 50)
-        
-    # def test_divide(self):             
-    #     self.assertEqual(calc.divide(10, 5), 2)
-    
-    
-    # def compat_input(self, s=''):
-    #     try:
-    #         return input(s)
-    #     except NameError:
-    #         return input(s)
+            
     def test_power(self):
-        self.assertEquat(pow(5, 2), 25)
+        self.assertEquals(pow(5, 2), 25)
  
  
  
