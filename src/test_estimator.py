2c8050f600db960cd34262070a61ebd737546053
Oswago Hesborn <oswago76@gmail.com>
2020-04-13 18:40:31 +0300

    Added new test for input function

--- src/test_estimator.py
+++ src/test_estimator.py
@@ -1,24 +1,31 @@
 
 import unittest
 from unittest.mock import patch
-import math
+# import calc
 from unittest import mock
 from unittest import TestCase
 
 
 class EstimatorTest(unittest.TestCase):
-    def test_divide(self):             
-        self.assertEqual(math.pow(2, 3), 8)
+    # def test_divide(self):             
+    #     self.assertEqual(calc.pow(2, 3), 8)
     
-    def test_add(self):             
-        self.assertEqual(math.add(10, 5), 15)
+    # def test_add(self):             
+    #     self.assertEqual(calc.add(10, 5), 15)
     
-    def test_multiply(self):             
-        self.assertEqual(math.multiply(10, 5), 50)
+    # def test_multiply(self):             
+    #     self.assertEqual(calc.multiply(10, 5), 50)
         
-    def test_divide(self):             
-        self.assertEqual(math.divide(10, 5), 2)
+    # def test_divide(self):             
+    #     self.assertEqual(calc.divide(10, 5), 2)
     
+    def test_input_mocking(self):
+        with unittest.mock.patch('builtins.input', return_value='y'):
+            assert input() == 'y'
+            print('we got here, so the ad hoc test succeeded')
+ 
+ 
+ 
  
 if __name__ == '__main__':
     unittest.main()
